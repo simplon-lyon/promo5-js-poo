04f1172cb9e20f49db2d53d1a8f82e62b2e986f2
Jean <demeljean@hotmail.fr>
2018-01-08 14:33:38 +0100

    Modified readme and added comments

--- www/scripts/form-person.js
+++ www/scripts/form-person.js
@@ -2,11 +2,14 @@
 
 // console.log(person);
 
+//On capture le formulaire
 const formulaire = document.querySelector('#formulaire');
-
+//On lui ajoute un event sur le submit
 formulaire.addEventListener('submit', function(event){
+    //On commence par stopper l'exécution par défaut du formulaire
     event.preventDefault();
 
+    //On récupère les différentes informations du formulaire
     let name = document.querySelector('#name').value;
 
     console.log(name);
